c8d983605165689ad6d64dcc07b6a12ec63ec941
lnfcode <leonardus.fernando@finansia.com>
2023-11-15 14:07:25 +0700

    fix: column type

--- models/entity/entity.go
+++ models/entity/entity.go
@@ -793,7 +793,7 @@ type TrxDetail struct {
 	NextStep       interface{} `gorm:"type:varchar(3);column:next_step" json:"-"`
 	Type           interface{} `gorm:"type:varchar(3);column:type" json:"-"`
 	Info           interface{} `gorm:"type:text;column:info" json:"-"`
-	Reason         interface{} `gorm:"type:text;column:reason" json:"reason"`
+	Reason         interface{} `gorm:"type:varchar(200);column:reason" json:"reason"`
 	CreatedBy      string      `gorm:"type:varchar(100);column:created_by" json:"-"`
 	CreatedAt      time.Time   `gorm:"column:created_at" json:"created_at"`
 }
