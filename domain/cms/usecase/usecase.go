82c98a6c5f50aef5ae2e0861763f598231e115a2
lnfcode <leonardus.fernando@finansia.com>
2024-05-02 11:57:53 +0700

    fix: remove has return ne

--- domain/cms/usecase/usecase.go
+++ domain/cms/usecase/usecase.go
@@ -1734,10 +1734,6 @@ func (u usecase) GetInquiryApproval(ctx context.Context, req request.ReqInquiryA
 
 		birthDate := inq.BirthDate.Format("02-01-2006")
 
-		if len(inq.ProspectID) > 2 && inq.ProspectID[0:2] == "NE" {
-			inq.HasReturn = true
-		}
-
 		row := entity.InquiryDataApproval{
 			CA: entity.DataApproval{
 				ShowAction:         inq.ShowAction,
