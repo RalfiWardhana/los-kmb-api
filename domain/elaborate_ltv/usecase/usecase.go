a799187620bb906e651b85c50cdac1e9ed2f9d10
hendradevelious <hendra.anugrah@kpvendor.id>
2024-06-11 10:13:05 +0700

    fix(KMB-2423) SELECT rrd_date & created_at on query `GetFilteringResult`

--- domain/elaborate_ltv/usecase/usecase.go
+++ domain/elaborate_ltv/usecase/usecase.go
@@ -117,6 +117,11 @@ func (u usecase) Elaborate(ctx context.Context, reqs request.ElaborateLTV, acces
 
 		// Cek apakah customer RO PRIME/PRIORITY ini termasuk jalur `expired_contract tidak <= 6 bulan`
 		if filteringKMB.CustomerStatus == constant.STATUS_KONSUMEN_RO {
+			if filteringKMB.RrdDate == nil {
+				err = errors.New(constant.ERROR_UPSTREAM + " - Customer RO then rrd_date should not be empty")
+				return
+			}
+
 			RrdDateString = filteringKMB.RrdDate.(string)
 			CreatedAtString = filteringKMB.CreatedAt.Format(time.RFC3339)
 
