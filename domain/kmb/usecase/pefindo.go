5e861c677968261cd5be526134148fe04cc24009
lnfcode <leonardus.fernando@finansia.com>
2025-05-06 11:39:27 +0700

    fix: ao priority di pefindo

--- domain/kmb/usecase/pefindo.go
+++ domain/kmb/usecase/pefindo.go
@@ -73,10 +73,10 @@ func (u usecase) Pefindo(cbFound bool, bpkbName string, filtering entity.Filteri
 	}
 
 	if customerSegment == constant.RO_AO_PRIME || customerSegment == constant.RO_AO_PRIORITY {
-		if spDupcheck.StatusKonsumen == constant.STATUS_KONSUMEN_AO && spDupcheck.InstallmentTopup == 0 && (spDupcheck.NumberOfPaidInstallment >= 6 || spDupcheck.AgreementSettledExist) {
+		if spDupcheck.StatusKonsumen == constant.STATUS_KONSUMEN_AO && spDupcheck.InstallmentTopup == 0 && (spDupcheck.NumberOfPaidInstallment >= 1 || spDupcheck.AgreementSettledExist) {
 			data = response.UsecaseApi{
 				Code:           constant.CODE_PEFINDO_PRIME_PRIORITY,
-				Reason:         fmt.Sprintf("%s %s >= 6 bulan - PBK Pass", spDupcheck.StatusKonsumen, customerSegment),
+				Reason:         fmt.Sprintf("%s %s - PBK Pass", spDupcheck.StatusKonsumen, customerSegment),
 				Result:         constant.DECISION_PASS,
 				SourceDecision: constant.SOURCE_DECISION_BIRO,
 			}
