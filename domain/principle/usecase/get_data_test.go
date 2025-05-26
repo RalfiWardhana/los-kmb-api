f33d012189d76106470e50dfa0fdde389bfa990c
LinggaWahyuR <lingga.wahyu@kpvendor.id>
2025-05-26 17:06:28 +0700

    fix: update response type referral_code 2wilen

--- domain/principle/usecase/get_data_test.go
+++ domain/principle/usecase/get_data_test.go
@@ -901,7 +901,7 @@ func TestGetDataPrinciple(t *testing.T) {
 				"baki_debet":                 sampleTrxKPM.BakiDebet,
 				"readjust_context":           sampleTrxKPM.ReadjustContext,
 				"rent_finish_date":           sampleTrxKPM.RentFinishDate,
-				"referral_code":              sampleTrxKPM.ReferralCode,
+				"referral_code":              sampleTrxKPM.ReferralCode.String,
 			},
 		},
 		{
